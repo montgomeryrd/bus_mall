a0b0b053d5cf21d487a31f548ad56ab4d880b5a4
Richard Montgomery <rikermonty@gmail.com>
2017-10-30 18:10:22 -0700

    Constructor Functions, Instantiated Objects, Random Number Generator and a Function to Compare Numbers

--- User Stories.md
+++ User Stories.md
@@ -4,11 +4,11 @@
 -As an introverted user who commutes by bus, I rely heavily on massive amounts of distraction to aid me in ignoring fellow commuters as well as my crippling claustrophobia. A catalog of items that hold my interest might help.
 
 -As a dev, I want to develop an app to best identify which products appeal to the mass of people who use the transit system.
--As a dev, I want to make an app that will catalog the number of times someone clicks on an image of a product for market analysis.
--As a student dev, I've been given the assignment to construct an app that will display three images at a time and every time a user clicks on an image from that selection, it will provide another set of images. At the end of my app, it will display the number of times an image was shown, and the number of times an image was selected.
--As a broke developer, I will take any job and produce any product as I have bills to pay and a corgi to feed. 
+-As a dev, I want the app to catalog the number of times someone clicks on an image of a product for market analysis.
+-As a student dev, I've been given the assignment to construct an app that will display three images at a time and every time a user clicks on an image from that selection, it will provide another set of images. At the end of my app, it will display the number of times an image was shown and the number of times an image was selected.
+-As a broke developer, I will take any job and produce any product as I have bills to pay and a corgi to feed.
 
--As a market researcher, I need some nerd to make me an app that shows what products people are most interested in to enter into the BusMall catalog on bus rides.
+-As a market researcher, I need an app that shows what products people are most interested in to enter into the BusMall catalog on bus rides.
 -As a market researcher, I need an analysis of products based on peak user interest. The app will need to record user selections to show levels of interest and what is worth displaying in BusMall's catalog.
--As a market researcher, I will need a tool to I can use again and again for future products to maintain a catalog users want to pick up and purchase products from.
+-As a market researcher, I will need a tool that I can use again and again for future products to maintain a catalog users want to pick up and purchase products from.
 -As a market researcher, I make a lot of money.
--- app.js
+++ app.js
@@ -1,1 +1,75 @@
 'use strict';
+
+//Array of images
+var images = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
+
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+
+
+one.setAttribute('src','Images/bag.jpg');
+console.log(one);
+
+
+//Constructor Function
+function Filepath(name, filepath) {
+  this.name = name;
+  this.filepath = filepath;
+}
+
+//Objects
+var bag = new Filepath('bag', 'Image/bag.jpg');
+var banana = new Filepath('banana', 'Image/banana.jpg');
+var bathroom = new Filepath('bathroom', 'Images/bathoom.jpg');
+var boots = new Filepath('boots', 'Images/boots.jpg');
+var breakfast = new Filepath('breakfast', 'Images/breakfast.jpg');
+var bubblegum = new Filepath('bubblegum', 'Images/bubblegum.jpg');
+var chair = new Filepath('chair', 'Images/chair.jpg');
+var cthulu = new Filepath('cthulu', 'Images/cthulu.jpg');
+var dogDuck = new Filepath('dog-duck', 'Images/dog-duck.jpg');
+var dragon = new Filepath('dragon', 'Images/dragon.jpg');
+var pen = new Filepath('pen', 'Images/pen.jpg');
+var petSweep = new Filepath('pet-sweep', 'Images/pet-sweep.jpg');
+var scissors = new Filepath('scissors', 'Images/scissors.jpg');
+var shark = new Filepath('shark', 'Images/shark.jpg');
+var sweep = new Filepath('sweep', 'Images/sweep.png');
+var tauntaun = new Filepath('tauntaun', 'Images/tauntaun.jpg');
+var unicorn = new Filepath('unicorn', 'Images/unicorn.jpg');
+var usb = new Filepath('usb', 'Images/usb.gif');
+var waterCan = new Filepath('water-can', 'Images/water-can.jpg');
+var wineGlass = new Filepath('wine-glass', 'Images/wine-glass');
+
+//My Ridiculous Random Number Generator
+var previousNumbers = [];
+var shownLast = [];
+
+function randomNums (){
+  var randomNumber = Math.floor(Math.random() * images.length);
+  previousNumbers.push(randomNumber);
+  randomNumber = Math.floor(Math.random() * images.length);
+  while (randomNumber === previousNumbers[0] || randomNumber === previousNumbers[1] || randomNumber === previousNumbers[2]) {
+    randomNumber = Math.floor(Math.random() * images.length);
+  }
+  previousNumbers.push(randomNumber);
+  randomNumber = Math.floor(Math.random() * images.length);
+  while (randomNumber === previousNumbers[0] || randomNumber === previousNumbers[1] || randomNumber === previousNumbers[2]) {
+    randomNumber = Math.floor(Math.random() * images.length);
+  }
+  previousNumbers.push(randomNumber);
+}
+
+randomNums();
+
+//These are the values that were shown last
+function checkNums(){
+  shownLast.push(previousNumbers);
+  previousNumbers = [];
+
+  for(var i = 0 ; i < 3 ; i++) {
+    randomNumber = Math.floor(Math.random() * images.length);
+    if (randomNumber !== shownLast[0] || randomNumber !== shownLast[1] || randomNumber !== shownLast[2]) {
+      previousNumbers.push(randomNumber);
+    }
+  }
+}
